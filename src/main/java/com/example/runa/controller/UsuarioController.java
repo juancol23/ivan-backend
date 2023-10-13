d10df955caeeb8f270e0d70d0100c5ba10e451cd
Colichon Ramirez <jcolichon@intercorp.com.pe>
2023-10-12 21:02:32 -0500

    mejorando login

--- src/main/java/com/example/runa/controller/UsuarioController.java
+++ src/main/java/com/example/runa/controller/UsuarioController.java
@@ -3,9 +3,12 @@ package com.example.runa.controller;
 import com.example.runa.model.Usuario;
 import com.example.runa.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UsuarioController {
@@ -17,4 +20,12 @@ public class UsuarioController {
         usuarioRepository.save(usuario);
         return usuario;
     }
+
+    @GetMapping("/user")
+    public List<Usuario> guardarUsuario() {
+        List<Usuario> listadoUsuarios =  usuarioRepository.findAll();
+        return listadoUsuarios;
+    }
+
+
 }
