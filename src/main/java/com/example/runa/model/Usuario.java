e7ef1550fc9fc12653667fba77858d5e37e22252
Colichon Ramirez <jcolichon@intercorp.com.pe>
2023-10-17 13:23:06 -0500

    a

--- src/main/java/com/example/runa/model/Usuario.java
+++ src/main/java/com/example/runa/model/Usuario.java
@@ -7,8 +7,6 @@ import lombok.Setter;
 
 
 @Entity
-@Getter
-@Setter
 public class Usuario {
     @Id
     private String nombreUsuario;
@@ -17,4 +15,43 @@ public class Usuario {
     private String email;
     private String status;//
 
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
